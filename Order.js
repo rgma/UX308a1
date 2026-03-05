418d32e43da3537276c0685000ef216ee70325f9
rgma <wagl1044@mylaurier.ca>
2026-03-05 19:07:51 +0000

    Updated Order.js

--- Order.js
+++ Order.js
@@ -1,5 +1,12 @@
 let currentState = welcoming;
 
+let order = {
+  item: "",
+  size: "", 
+  topping: "", 
+  upsell: ""
+};
+
 export function handleInput(sInput) {
   return currentState(sInput);
 }
@@ -10,24 +17,77 @@ export function clearInput(){
 
 function welcoming() {
   let aReturn = [];
-  currentState = reserving;
-  aReturn.push("Welcome to Rich's Acton Rapid Test.");
-  aReturn.push("Would you like to reserve a rapid test kit?");
+  currentState = chooseItem;
+
+  aReturn.push("Welcome to The Boba Bar.");
+  aReturn.push("What would you like?");
+  aReturn.push("1. Bubble Tea");
+  aReturn.push("2. Fruit Smoothie");
+
   return aReturn;
 }
 
-function reserving(sInput) {
+function chooseItem(sInput) {
   let aReturn = [];
-  currentState = welcoming
-  if (sInput.toLowerCase().startsWith('y')) {
-    aReturn.push(`Your rapid test is reserved`);
-    let d = new Date();
-    d.setMinutes(d.getMinutes() + 120);
-    aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
-  } else {
-    aReturn.push("Thanks for trying our reservation system");
-    aReturn.push("Maybe next time");
+    
+  if(sInput == "1"){
+    order.item = "Bubble Tea"; 
+    currentState = chooseSize;
+    aReturn.push("Great! Boba it is!");
+    aReturn.push("Choose a size: Small or Large");
   }
+  else if(sInput == "2"){
+    order.item = "Fruit Smoothie"; 
+    currentState = chooseSize;
+    aReturn.push("Great! Fruit Smoothie it is!");
+    aReturn.push("Choose a size: Small or Large");
+  }
+  else{
+    aReturn.push("Please type 1 or 2");
+  }
+
   return aReturn;
 }
 
+function chooseSize(sInput) {
+  let aReturn = [];
+
+  order.size = sInput; 
+  currentState = chooseTopping; 
+
+  aReturn.push("Pick a topping:");
+  aReturn.push("Pearls or Jelly");
+
+  return aReturn;
+}
+
+function chooseTopping(sInput) {
+  let aReturn = [];
+
+  order.topping = sInput; 
+  currentState = upsell; 
+
+  aReturn.push("Would you like to add a cookie? (y/n)");
+
+  return aReturn;
+}
+    
+function upsell(sInput){
+  let aReturn = [];
+
+  if(sInput.toLowerCase() == "y") {
+    order.upsell = "Cookie";
+  } 
+  else {
+    order.upsell = "No Cookie";
+  }
+
+  currentState = welcoming;
+
+  aReturn.push("Order Confirmed!");
+  aReturn.push(`${order.size} ${order.item} with ${order.topping}`);
+  aReturn.push(`Extra: ${order.upsell}`);
+  aReturn.push("Your order will be ready soon!");
+
+  return aReturn;
+}
